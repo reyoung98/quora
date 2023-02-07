1c3860961a87a2555de97eeff66308cbde29fb2f
Rachel Young <reyoung255@gmail.com>
2023-02-07 15:14:30 +0100

    Adding comment section

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react"
 import { Likes } from './Likes'
 
 export const Answer = (props) => {
+    const [input, setInput] = useState('')
+    const [comment, setComment] = useState('')
+
+    const handleInput = (e) => {
+        setInput(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setComment(input);
+        setInput('');
+    }
+
+
     return (
         <>
-            <p>{props.answer}</p>
+         <p>{props.answer}</p>
             <Likes />
+            {
+                comment !== '' && (
+                    <p>{comment}</p>
+                ) 
+            }
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={input} onChange={handleInput}/>
+                <button type="submit">Add comment</button>
+            </form>
+           
         </>
     )
 }
